dbc17a30f27beec77ddda9e1b91bd7aa9b0c1525
David Valachovic <David.E.Valachovic@gmail.com>
2018-10-14 02:57:21 -0500

    mo net stuff

---
+++ Assets/Scripts/Network/SnakeNetworkManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SnakeSpawnMessage : MessageBase {
+
+}
+
+public class SnakeNetworkManager : NetworkManager {
+
+	// Use this for initialization
+	void Start() { }
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	const short MsgTypeSnakeSpawn = 1000;
+
+	#region Client
+	#endregion
+
+	//Detect when a client connects to the Server
+	public override void OnClientConnect(NetworkConnection connection) {
+		base.OnClientConnect(connection);
+		//Change the text to show the connection on the client side
+		Debug.Log(connection.connectionId + " Connected!");
+		var snake = Instantiate(playerPrefab);
+		snake.GetComponent<Snake>().isLocalPlayer = true;
+		client.Send(MsgTypeSnakeSpawn, new SnakeSpawnMessage());
+	}
+
+	//Detect when a client connects to the Server
+	public override void OnClientDisconnect(NetworkConnection connection) {
+		base.OnClientDisconnect(connection);
+		//Change the text to show the connection loss on the client side
+		Debug.Log("Connection" + connection.connectionId + " Lost!");
+	}
+
+	public override void OnStartClient(NetworkClient client) {
+		Debug.Log("OnStartClient");
+		client.RegisterHandler(MsgTypeSnakeSpawn, OnServerSnakeSpawn);
+	}
+
+	public override void OnStartServer() {
+		Debug.Log("OnStartServer");
+		NetworkServer.RegisterHandler(MsgTypeSnakeSpawn, OnServerSnakeSpawn);
+	}
+
+	public void OnServerSnakeSpawn(NetworkMessage networkMessage) {
+		Debug.Log("OnServerSnakeSpawn: networkMessage " + JsonUtility.ToJson(networkMessage));
+		Debug.Log("OnServerSnakeSpawn: networkMessage.conn " + JsonUtility.ToJson(networkMessage.conn));
+		Debug.Log("OnServerSnakeSpawn: networkMessage.conn.connectionId " + networkMessage.conn.connectionId);
+		Debug.Log("OnClientSnakeSpawn: " + networkMessage);
+		var isMessageFromHost = networkMessage.conn.connectionId == 0;
+		Debug.Log("OnServerSnakeSpawn: NetworkServer.localConnections " + String.Join(", ", NetworkServer.localConnections.Select(JsonUtility.ToJson).ToArray()));
+		Debug.Log("OnServerSnakeSpawn: NetworkServer.connections " + String.Join(", ", NetworkServer.connections.Select(JsonUtility.ToJson).ToArray()));
+
+		var snakeSpawnMessage = networkMessage.ReadMessage<SnakeSpawnMessage>();
+		var connectionIdsWithHost = NetworkServer.connections.Select(x => x.connectionId).ToList();
+		connectionIdsWithHost.Add(0);
+		Debug.Log("OnServerSnakeSpawn: connectionIdsWithHost.Count " + connectionIdsWithHost.Count);
+		Debug.Log("OnServerSnakeSpawn: connectionIdsWithHost " + String.Join(", ", connectionIdsWithHost.Select(x => x.ToString()).ToArray()));
+		var filteredConnectionIds = connectionIdsWithHost.Where(x => x != networkMessage.conn.connectionId).ToList();
+		Debug.Log("OnServerSnakeSpawn: filteredConnectionIds " + String.Join(", ", filteredConnectionIds.Select(x => x.ToString()).ToArray()));
+		filteredConnectionIds.ForEach(x => NetworkServer.SendToClient(x, MsgTypeSnakeSpawn, snakeSpawnMessage));
+	}
+
+	public void OnClientSnakeSpawn(NetworkMessage networkMessage) {
+		Debug.Log("OnClientSnakeSpawn: " + networkMessage);
+		var y = networkMessage.ReadMessage<SnakeSpawnMessage>();
+		Instantiate(playerPrefab);
+	}
+}
--- Assets/Scripts/Snake/Snake.cs
+++ Assets/Scripts/Snake/Snake.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
 
-public class Snake : NetworkBehaviour {
+public class Snake : MonoBehaviour {
 
     public static List<Snake> snakes = new List<Snake>();
 
@@ -21,6 +21,7 @@ public class Snake : NetworkBehaviour {
     Direction direction = Up.I;
     Direction nextDirection = Up.I;
     public ISnakeController controller;
+    public bool isLocalPlayer = false;
 
     void Awake() {
         snakes.Add(this);
@@ -29,21 +30,20 @@ public class Snake : NetworkBehaviour {
     }
 
     void Update() {
-        if (!isLocalPlayer) {
-            return;
-        }
+        if (isLocalPlayer) {
 
-        if (direction != Down.I && controller.IsUpButtonPressed()) {
-            nextDirection = Up.I;
-        }
-        if (direction != Left.I && controller.IsRightButtonPressed()) {
-            nextDirection = Right.I;
-        }
-        if (direction != Up.I && controller.IsDownButtonPressed()) {
-            nextDirection = Down.I;
-        }
-        if (direction != Right.I && controller.IsLeftButtonPressed()) {
-            nextDirection = Left.I;
+            if (direction != Down.I && controller.IsUpButtonPressed()) {
+                nextDirection = Up.I;
+            }
+            if (direction != Left.I && controller.IsRightButtonPressed()) {
+                nextDirection = Right.I;
+            }
+            if (direction != Up.I && controller.IsDownButtonPressed()) {
+                nextDirection = Down.I;
+            }
+            if (direction != Right.I && controller.IsLeftButtonPressed()) {
+                nextDirection = Left.I;
+            }
         }
 
         elapsedTime += Time.deltaTime;
@@ -63,13 +63,11 @@ public class Snake : NetworkBehaviour {
 
             if (growOnNextMove) {
                 growOnNextMove = false;
-                //var newLink = GameObject.Instantiate(snakeTailPrefab);
+                var newLink = GameObject.Instantiate(snakeTailPrefab);
 
-                SpawnObject();
-
-                //newTail.transform.parent = transform;
-                //newTail.transform.position = head.transform.position;
-                //links.Insert(1, newTail);
+                newLink.transform.parent = transform;
+                newLink.transform.position = head.transform.position;
+                links.Insert(1, newLink);
 
             } else if (links.Count > 1) {
                 var oldTail = links[links.Count - 1];
@@ -81,22 +79,6 @@ public class Snake : NetworkBehaviour {
             head.transform.rotation = direction.GetHeadRotation();
             head.transform.position = newPosition;
         }
-    }
-
-    GameObject newTail;
-    [Client] // called only on client
-    public void SpawnObject()
-    {
-        CmdSpawn();
-        newTail.transform.position = head.transform.position;
-        links.Insert(1, newTail);
-    }
-
-    [Command] // runs only on server
-    private void CmdSpawn()
-    {
-        newTail = Instantiate(snakeTailPrefab, Vector3.zero, Quaternion.identity);
-        NetworkServer.Spawn(newTail);
     }
 
     private bool AboutToCollideWithSelf(Vector3 newPosition) {
@@ -122,9 +104,9 @@ public class Snake : NetworkBehaviour {
     }
 
     void OnTriggerEnter(Collider other) {
-        //if (other.gameObject.HasComponent<Wall>() || (other.gameObject.HasComponent<SnakeTail>())) {
-        //    Die();
-        //}
+        if (other.gameObject.HasComponent<Wall>() || (other.gameObject.HasComponent<SnakeTail>())) {
+            Die();
+        }
         if (other.gameObject.HasComponent<Apple>()) {
             Destroy(other.gameObject);
             Grow();
